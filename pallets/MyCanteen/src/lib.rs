bc3241282324a5146a5cee3fc0dcddef40a2dcfb
V778 <83821496+V778@users.noreply.github.com>
2021-07-27 10:37:28 +0530

    Add files via upload

---
+++ pallets/MyCanteen/src/lib.rs
@@ -0,0 +1,181 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://substrate.dev/docs/en/knowledgebase/runtime/frame>
+
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
+	use frame_system::pallet_prelude::*;
+
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	// The pallet's runtime storage items.
+	// https://substrate.dev/docs/en/knowledgebase/runtime/storage
+	#[pallet::storage]
+	#[pallet::getter(fn something)]
+	// Learn more about declaring storage items:
+	// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
+	pub type Something<T> = StorageValue<_, u32>;
+
+	// Pallets use events to inform users when important changes are made.
+	// https://substrate.dev/docs/en/knowledgebase/runtime/events
+	#[pallet::event]
+	#[pallet::metadata(T::AccountId = "AccountId")]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// parameters. [something, who]
+		SomethingStored(u32, T::AccountId),
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Error names should be descriptive.
+		NoneValue,
+		/// Errors should have helpful documentation associated with them.
+		StorageOverflow,
+	}
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T:Config> Pallet<T> {
+		
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn do_something(origin: OriginFor<T>, a: u32,b: u32) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+			let who = ensure_signed(origin)?;
+			let res = a + b;
+
+			// Update storage.
+			<Something<T>>::put(res);
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored(res, who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn menu(origin: OriginFor<T>,paneer:u32) -> DispatchResult
+		 {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+			let who = ensure_signed(origin)?;
+			let a:u32=paneer;
+
+			// Update storage.
+			<Something<T>>::put(a);
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored(a,who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+		
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn menu1(origin: OriginFor<T>,chana_Masala:u32) -> DispatchResult
+		 {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+			let who = ensure_signed(origin)?;
+			let b:u32 = chana_Masala;
+
+			// Update storage.
+			<Something<T>>::put(b);
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored(b,who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+		
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn menu2(origin: OriginFor<T>,kofta:u32) -> DispatchResult
+		 {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+			let who = ensure_signed(origin)?;
+			let c:u32=kofta;
+
+			// Update storage.
+			<Something<T>>::put(c);
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored(c,who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+		
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn menu3(origin: OriginFor<T>,kashmiri_dum:u32) -> DispatchResult
+		 {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+			let who = ensure_signed(origin)?;
+			let d:u32=kashmiri_dum;
+			
+
+			// Update storage.
+			<Something<T>>::put(d);
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored(d,who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+		/// An example dispatchable that may throw a custom error.
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
+		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
+			let _who = ensure_signed(origin)?;
+
+			// Read a value from storage.
+			match <Something<T>>::get() {
+				// Return an error if the value has not been set.
+				None => Err(Error::<T>::NoneValue)?,
+				Some(old) => {
+					// Increment the value read from storage; will error in the event of overflow.
+					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					// Update the value in storage with the incremented result.
+					<Something<T>>::put(new);
+					Ok(())
+				},
+			}
+		}
+	}
+}
